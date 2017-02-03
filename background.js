c7f245bd5c655813073447ca7d298ceae28c00bf
Saikat Guha <sktguha@gmail.com>
2017-02-03 21:57:46 +0530

    full download not automated yet

--- background.js
+++ background.js
@@ -1,17 +1,17 @@
 chrome.commands.onCommand.addListener(function (command) {
     chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-	   var url = "http://en.savefrom.net/#url=" + tabs[0].url;
+	   var url = "http://www.onlinevideoconverter.com/mp3-converter?url=" + tabs[0].url;
        
 	   chrome.tabs.create({ url: url }, function(tab){
-		    //chrome.tabs.update(tabs[0].id, {selected: true});
+		    chrome.tabs.update(tabs[0].id, {selected: true});
 			chrome.tabs.executeScript(
 				tab.id,
 				{code: sc }
 			);
-			setTimeout(function(){
+			/*setTimeout(function(){
 				chrome.tabs.remove(tab.id, function() { });
-			},7000);
+			},12000);*/
 	   });
 	});
 });
-var sc = "var id = setInterval(function(){ var el = document.querySelectorAll('[data-type=\"Audio MP4\"]')[0]; if(el){ el.click(); clearInterval(id); }}, 200); chr";
+var sc = "setTimeout(function(){ document.getElementsByClassName('start-button')[0].click() } , 1500);";
